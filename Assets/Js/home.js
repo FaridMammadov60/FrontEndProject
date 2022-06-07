59f8b288e8a8c2cb49a136bf262f88d262c04ec4
FaridMammadov60 <faridmma@code.edu.az>
2022-06-07 06:47:50 -0700

    add: home

--- Assets/Js/home.js
+++ Assets/Js/home.js
@@ -1,13 +1,131 @@
-let locationHead=document.getElementById("location-head");
+let btnchart = document.querySelectorAll(".btnchart");
+let productCount = document.getElementById("productCount");
+let productUsd = document.getElementById("productUsd");
 
-$(document).ready(function (){
-    $("#location-head").click(function (){
-        swal({
-            title: "Choose your Delivery Location",
-          });
+btnchart.forEach(btn => {
+    btn.addEventListener("click", function (ev) {
+        ev.preventDefault();
+        if (localStorage.getItem("basket") == null) {
+            localStorage.setItem("basket", JSON.stringify([]));            
+        }
+        
+        let arr = JSON.parse(localStorage.getItem("basket"));               
+        let productId = this.parentElement.getAttribute("data-id");
+        let existProductId = arr.find(p => p.id == productId);
+        if (existProductId == undefined) {
+            arr.push({
+                id: productId,
+                price: this.previousElementSibling.firstElementChild.firstElementChild.innerText,
+                imageUrl: this.parentElement.firstElementChild.getAttribute("src"),
+                name: this.parentElement.firstElementChild.innerText,
+                count: 1
+
+            })
+        }
+        else{
+            existProductId.count++;
+        }
+       
+        localStorage.setItem("basket",JSON.stringify(arr));
+        WriteProductCount();
     })
 })
 
+function WriteProductCount(){
+    if(localStorage.getItem("basket")!=null){
+        let arr=JSON.parse(localStorage.getItem("basket"));
+        let totalCount=0;
+        
+        arr.map(product=>{
+            totalCount+=product.count;    
+            
+        })
+        productCount.innerText=totalCount;
+
+        let totalPrice=0;        
+        arr.forEach(product=>{
+            totalPrice+=product.count* parseFloat(product.price);
+        })
+        console.log(totalPrice);
+        productUsd.innerText=totalPrice
+    }
+}
+WriteProductCount();
+$(document).ready(function () {
+    $(".location").click(function () {
+        swal({
+            title: "Choose your Delivery Location",
+        });
+    })
+
+    // deadline start
+
+    $('#deadline').countdown('2022/8/12', function (event) {
+
+        // $('#days').html(event.strftime(''
+        //     + `<div>${66}</div>`
+        // ));
+        $('#hours').html(event.strftime(''
+            + `<div>%H</div>`
+        ));
+        $('#minutes').html(event.strftime(''
+            + `<div>%M</div>`
+        ));
+        $('#sec').html(event.strftime(''
+            + `<div>%S</div>`
+        ));
+    });
+
+    (function () {
+        let second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        //I'm adding this section so I don't have to keep updating this pen every year :-)
+        //remove this if you don't need it
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy,
+            dayMonth = "08/12/",
+            deadline = dayMonth + yyyy;
+
+        today = mm + "/" + dd + "/" + yyyy;
+        if (today > deadline) {
+            deadline = dayMonth + nextYear;
+        }
+        //end
+
+        let countDown = new Date(deadline).getTime(),
+            x = setInterval(function () {
+
+                let now = new Date().getTime(),
+                    distance = countDown - now;
+
+                document.getElementById("days").innerText = Math.floor(distance / (day));
+                // document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                // document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                // document.getElementById("sec").innerText = Math.floor((distance % (minute)) / second);
+
+                //do something later when date is reached
+                if (distance < 0) {
+                    clearInterval(x);
+                }
+                //seconds
+            }, 0)
+    }());
+
+
+    // deadline end
+
+})
+
+
+
+
+//  Carousel Section Start
 
 let index = 0,
     amount = 0,
@@ -43,7 +161,6 @@ document.addEventListener("DOMContentLoaded", function () {
 function prev() {
     if (translationComplete === true) {
         translationComplete = false;
-        console.log(index);
 
         index--;
         if (index == -1) {
@@ -67,7 +184,6 @@ function next() {
     if (translationComplete === true) {
         translationComplete = false;
         let outerIndex = (index) % amount;
-        console.log(index);
         index++;
         for (let i = 0; i < amount; i++) {
             let slide = document.getElementsByClassName("slide")[i];
@@ -80,4 +196,52 @@ function next() {
         outerSlide.style.opacity = '0';
         currTransl[outerIndex] = currTransl[outerIndex] + moveOffset * (amount);
     }
+}
+
+//  Carousel Section End
+
+
+// product carousel start 
+let items = document.querySelectorAll('#featurecntnr .carousel .carousel-item');
+items.forEach((el) => {
+    const minPerSlide = 5
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+$(document).ready(function () {
+    $('#featureCarousel').carousel({ interval: false });
+    $('#featureCarousel').carousel('pause');
+});
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+try {
+    fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function (response) {
+        return true;
+    }).catch(function (e) {
+        var carbonScript = document.createElement("script");
+        carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+        carbonScript.id = "_carbonads_js";
+        document.getElementById("carbon-block").appendChild(carbonScript);
+    });
+} catch (error) {
+    console.log(error);
 }
